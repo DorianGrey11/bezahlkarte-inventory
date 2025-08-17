6e1a96f3cb91a4cda4da761c02bd5657db8178b9
DorianGrey11 <doriangrey11@posteo.eu>
2025-08-17 17:12:13 +0200

    feat: add updated_at to api collections

--- core/views/ApiInventoryView.py
+++ core/views/ApiInventoryView.py
@@ -1,3 +1,4 @@
+from django.db.models import Max
 from django.views import View
 from django.http import JsonResponse
 
@@ -6,13 +7,18 @@ from core.models import Collection
 
 class ApiInventoryView(View):
     def get(self, request):
-        collections = Collection.objects.prefetch_related('accounts').filter(show_in_api=True)
+        collections = (Collection.objects.prefetch_related('accounts')
+                       .filter(show_in_api=True)
+                       .annotate(updated_at=Max('accounts__transactions__created_at')))
         inventory = {
             collection.name: {
-                account.name: {
-                    "amount": account.number_of_gift_cards,
-                    "total_value": account.balance
-                } for account in collection.accounts.filter(type="gift_card") if account.balance > 0
+                "gift_cards": {
+                    account.name: {
+                        "amount": account.number_of_gift_cards,
+                        "total_value": account.balance
+                    } for account in collection.accounts.filter(type="gift_card") if account.balance > 0
+                },
+                "updated_at": collection.updated_at,
             } for collection in collections
         }
         return JsonResponse(inventory)
